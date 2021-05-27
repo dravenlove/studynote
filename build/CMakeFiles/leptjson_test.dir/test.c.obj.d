a083ae17871bc6649357204e45f69b071d2a5f4d
dravenlove <13725162400@163.com>
2021-05-28 02:04:44 +0800

    5.28.2.00

---
+++ build/CMakeFiles/leptjson_test.dir/test.c.obj.d
@@ -0,0 +1,24 @@
+CMakeFiles/leptjson_test.dir/test.c.obj: \
+ C:\Users\draven\Desktop\work\SSH\desktop\html\interesting\day1\tutorial01\test.c \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/stdlib.h \
+ C:/environment/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include-fixed/limits.h \
+ C:/environment/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include-fixed/syslimits.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/limits.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/sec_api/stdlib_s.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/malloc.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/string.h \
+ C:/environment/mingw64/x86_64-w64-mingw32/include/sec_api/string_s.h \
+ C:\Users\draven\Desktop\work\SSH\desktop\html\interesting\day1\tutorial01\leptjson.h
