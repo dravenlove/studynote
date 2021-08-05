ed728e7d0cbb0a02abcbd59ea729a119bda691d0
dravenlove <13725162400@163.com>
2021-08-05 14:34:58 +0800

    213

---
+++ .history/work/day21/Hello_20210805100540.erl
@@ -0,0 +1,4 @@
+-module(Hello). 
+-export([start/0]). 
+start() ->
+        io:format("hello world-n")
---
+++ work/day21/afile_server.erl
@@ -0,0 +1,16 @@
+    -module(afile_server). 
+    -export([start/1,loop/1]). 
+
+    start(Dir) -> spawn(afile_server, loop,[Dir]). 
+
+    loop(Dir) ->
+        %%等待指令
+        receive
+            {Client, list_dir} ->
+            %%需要做什么
+                Client ! {self(),file:list_dir(Dir)};
+            {Client,{get_file,File}} ->
+                Full = filename:join(Dir, File),
+                Client ! {self(), file:read_file(Full)}
+        end,
+        loop(Dir). 
